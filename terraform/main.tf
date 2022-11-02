c16b4c763ab30f3ace7c1a4610c868de35821e98
John Martin <jem2305@gmail.com>
2022-11-02 11:51:16 -0500

    update terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,18 +21,17 @@ resource "random_string" "bucket_id" {
   upper             = false
 }
 
-resource "ibm_resource_instance" "cos_instance" {
+data "ibm_resource_instance" "cos_instance" {
   name              = "CloudObjectStorage"
   resource_group_id = data.ibm_resource_group.default_group.id
   service           = "cloud-object-storage"
-  plan              = "lite"
   location          = "global"
 }
 
 resource "ibm_cos_bucket" "policy_as_code_bucket" {
   bucket_name          = "policyascode${random_string.bucket_id.result}"
-  resource_instance_id = ibm_resource_instance.cos_instance.id
+  resource_instance_id = data.ibm_resource_instance.cos_instance.id
   region_location      = "us-south"
   storage_class        = "standard"
-  endpoint_type        = "private"
+  endpoint_type        = "direct"
 }
