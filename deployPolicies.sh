9eb2fc6deea4720c7242032b89d35f5a6eb10af9
John Martin <jem2305@gmail.com>
2022-11-08 10:38:04 -0600

    remove scripts

--- deployPolicies.sh
+++
@@ -1,5 +0,0 @@
-opa build -b policies
-INSTANCE_ID=$(ibmcloud resource service-instance CloudObjectStorage --id | awk -F ' ' '$2 ~ /^[0-9]/ {print $2}')
-ibmcloud cos config crn $INSTANCE_ID
-ibmcloud cos object-put --bucket policyascodejm --key bundle.tar.gz --body bundle.tar.gz
-rm bundle.tar.gz
